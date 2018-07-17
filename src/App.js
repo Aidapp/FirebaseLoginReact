d1669c48ce3b43a5f0443232fd550dbc6c6ec390
Piyush Mehta <me@piyushmehta.com>
2018-07-17 23:04:27 +0530

    Onchange Handler
    Signed-off-by: Piyush Mehta <me@piyushmehta.com>

--- src/App.js
+++ src/App.js
@@ -3,7 +3,21 @@ import React, { Component } from "react";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {email:"",password:""};
+    this.MailChange = this.MailChange.bind(this);
+    this.PasswordChange = this.PasswordChange.bind(this);
+  }
+  // we console to check if it is working or not
+  MailChange(e)
+  {
+    console.log(e.target.value);
+    this.setState({email:e.target.value});
+  }
+  // we console to check if it is working or not
+  PasswordChange(e)
+  {
+     console.log(e.target.value);
+     this.setState({password:e.target.value});
   }
   render() {
     return (
@@ -11,9 +25,9 @@ class App extends Component {
         <center>
           <h1>Sign In REST API Login</h1>
         </center>
-        <input placeholder="Email" type="email"/>
+        <input placeholder="Email" type="email" onChange={this.MailChange}/>
         <br />
-        <input placeholder="Password" type="password" />
+        <input placeholder="Password" type="password"  onChange={this.PasswordChange}/>
         <button type="submit" >Submit</button>
       </div>
     );
